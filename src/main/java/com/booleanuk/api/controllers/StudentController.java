1290506eb75c49822f6ac26ae940e86d757229a7
alinjo <aliehsani99@yahoo.com>
2024-09-18 15:20:24 +0200

    finished

--- src/main/java/com/booleanuk/api/controllers/StudentController.java
+++ src/main/java/com/booleanuk/api/controllers/StudentController.java
@@ -11,7 +11,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/students")
+@RequestMapping("students")
 public class StudentController {
 
     @Autowired
